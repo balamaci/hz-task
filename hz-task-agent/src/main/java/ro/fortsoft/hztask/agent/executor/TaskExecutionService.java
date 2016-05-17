073389bb41a760716f5a4135d19d98f3fb4d0898
Serban Balamaci <serban.balamaci@1and1.ro>
2016-05-17 11:46:42 +0300

    pom.xml: add javaslang dependency

--- hz-task-agent/src/main/java/ro/fortsoft/hztask/agent/executor/TaskExecutionService.java
+++ hz-task-agent/src/main/java/ro/fortsoft/hztask/agent/executor/TaskExecutionService.java
@@ -1,11 +1,7 @@
 package ro.fortsoft.hztask.agent.executor;
 
 import com.google.common.eventbus.EventBus;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
+import javaslang.concurrent.Future;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ro.fortsoft.hztask.agent.event.task.TaskFailedEvent;
@@ -15,7 +11,7 @@ import ro.fortsoft.hztask.common.task.Task;
 import ro.fortsoft.hztask.common.task.TaskKey;
 
 import java.io.Serializable;
-import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
@@ -29,38 +25,24 @@ public class TaskExecutionService {
 
     private EventBus eventBus;
 
-    /**
-     * Pool for executing the tasks
-     */
-    private ListeningExecutorService taskExecutorService = MoreExecutors.
-            listeningDecorator(Executors.newCachedThreadPool());
+    /** Pool for executing the tasks */
+    private ExecutorService taskExecutorService = Executors.newCachedThreadPool();
 
     public TaskExecutionService(EventBus eventBus) {
         this.eventBus = eventBus;
     }
 
     public void executeTask(final TaskProcessor taskProcessor, final TaskKey taskKey, final Task task) {
-
-        ListenableFuture resultFuture = taskExecutorService.submit(new Callable() {
-            @Override
-            public Object call() throws Exception {
-                return taskProcessor.process(task);
-            }
-        });
-
-        Futures.addCallback(resultFuture, new FutureCallback() {
-            @Override
-            public void onSuccess(Object result) {
-                log.info("SUCCESS FINISH processing for task {}", task);
-                eventBus.post(new TaskFinishedEvent(taskKey, task, (Serializable) result));
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                log.info("FAIL FINISH processing for task {}", task.getId());
-                eventBus.post(new TaskFailedEvent(taskKey, throwable));
-            }
-        });
+        Future<? extends Serializable> resultFuture = Future.of(taskExecutorService, ()-> taskProcessor.process(task));
+        resultFuture
+                .onSuccess((result) -> {
+                    log.info("SUCCESS FINISH processing for task {}", task);
+                    eventBus.post(new TaskFinishedEvent(taskKey, task, result));
+                })
+                .onFailure((throwable) -> {
+                    log.info("FAIL FINISH processing for task {}", task.getId());
+                    eventBus.post(new TaskFailedEvent(taskKey, throwable));
+                });
     }
 
 
---
+++ hz-task-master/src/main/java/ro/fortsoft/hztask/master/router/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Strategies for routing Tasks to Agents
+ *
+ * @author sbalamaci
+ */
+package ro.fortsoft.hztask.master.router;
